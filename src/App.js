50fbb118d3f45625e8b451bc608611a6d5e7a326
ByeonghwiJeong <byeonghwijeong@gmail.com>
2023-01-27 01:32:01 +0900

    feat : onChange & e.target.value & 입력값

--- src/App.js
+++ src/App.js
@@ -12,10 +12,7 @@ function App() {
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
-
-  function 함수() {
-    console.log(1);
-  }
+  let [입력값, 입력값변경] = useState();
 
   return (
     <div className="App">
@@ -30,32 +27,6 @@ function App() {
         }}>
         글수정
       </button>
-      {/* <div className="list">
-        <h4>
-          {글제목[0]}{" "}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}>
-            👍
-          </span>{" "}
-          {따봉}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal(true);
-          }}>
-          {글제목[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div> */}
       {글제목.map(function (a, i) {
         return (
           <div className="list">
@@ -79,6 +50,14 @@ function App() {
           </div>
         );
       })}
+
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+          console.log(입력값);
+        }}
+      />
+
       {modal === true ? (
         <Modal title={title} 글제목변경={글제목변경} 글제목={글제목}></Modal>
       ) : null}
