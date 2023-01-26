68a463ff7e56db1dc69e225390f05ad2534db61c
ByeonghwiJeong <byeonghwijeong@gmail.com>
2023-01-27 00:56:59 +0900

    feat : map으로 게시판 리스트 각각 좋아요 구현

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { useState } from "react";
 function App() {
   let post = "강남 우동 맛집";
   let [글제목, 글제목변경] = useState([
-    "남자코드 추천",
+    "남자코트 추천",
     "강남 우동맛집",
     "파이썬 독학",
   ]);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
   function 함수() {
@@ -29,7 +29,7 @@ function App() {
         }}>
         글수정
       </button>
-      <div className="list">
+      {/* <div className="list">
         <h4>
           {글제목[0]}{" "}
           <span
@@ -54,7 +54,30 @@ function App() {
           {글제목[2]}
         </h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+      {글제목.map(function (a, i) {
+        return (
+          <div className="list">
+            <h4
+              onClick={() => {
+                setModal(true);
+              }}>
+              {a}
+              <span
+                onClick={() => {
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy);
+                }}>
+                👍
+              </span>
+              {따봉[i]}
+            </h4>
+            <h4>{글제목[i]}</h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
       {modal === true ? <Modal></Modal> : null}
     </div>
   );
