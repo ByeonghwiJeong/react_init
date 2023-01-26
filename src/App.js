1ff92090f4eab1303f93229062867d0303fc38c0
ByeonghwiJeong <byeonghwijeong@gmail.com>
2023-01-26 16:12:52 +0900

    feat : useState Array자료형 스프레드 복사후 변경

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [글제목, b] = useState(["남자코드 추천", "강남 우동맛집", "파이썬 독학"]);
+  let [글제목, 글제목변경] = useState([
+    "남자코드 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
   let [따봉, 따봉변경] = useState(0);
 
   function 함수() {
@@ -16,6 +20,14 @@ function App() {
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여자코트 추천";
+          글제목변경(copy);
+        }}>
+        글수정
+      </button>
       <div className="list">
         <h4>
           {글제목[0]}{" "}
